89f344ac698fd5d28799dabb8bfeb1375d022ba4
Ayman-Hachkal <ayman.hachkal@gmail.com>
2025-12-23 10:19:18 +0400

    setup file structure for advent of code

---
+++ 2025/Day1/out/Debug/CMakeFiles/AdventOfCode.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "AdventOfCode"
+  "AdventOfCode.pdb"
+  "CMakeFiles/AdventOfCode.dir/main.cpp.o"
+  "CMakeFiles/AdventOfCode.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AdventOfCode.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
